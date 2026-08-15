25e28b0be3e412a21fb1d39a099ad2b835e53bed
vivab <vivogaga12@gmail.com>
2026-08-16 02:27:42 +0500

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -6,7 +6,9 @@ from aiogram import Bot, Dispatcher
 TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
-    raise RuntimeError("BOT_TOKEN не найден")
+    raise RuntimeError(
+        "BOT_TOKEN не найден. Проверь настройки BotHost."
+    )
 
 
 bot = Bot(token=TOKEN)
